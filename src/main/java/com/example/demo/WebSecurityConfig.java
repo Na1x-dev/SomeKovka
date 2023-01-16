2ea4996c81c1c4fb9ff0acf5fda07e2d4099453a
na1x <na1x.1024@gmail.com>
2023-01-16 13:19:48 +0300

    first commit

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -18,9 +18,9 @@ import javax.annotation.PostConstruct;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Qualifier("userDetailsServiceImpl")
-    @Autowired
-    private UserDetailsService userDetailsService;
+//    @Qualifier("userDetailsServiceImpl")
+//    @Autowired
+//    private UserDetailsService userDetailsService;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -31,25 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/css/**", "/js/**", "/signUpPage/index").permitAll()
-                .antMatchers("/mainPage/index").access("hasRole('ROLE_USER')")
-                .antMatchers("/clientsPage/index").access("hasRole('ROLE_USER')")
-                .antMatchers("/newSalePage/index").access("hasRole('ROLE_USER')")
-                .antMatchers("/newSupplyPage/index").access("hasRole('ROLE_USER')")
-                .antMatchers("/salesPage/index").access("hasRole('ROLE_USER')")
-                .antMatchers("/suppliesPage/index").access("hasRole('ROLE_USER')")
-                .antMatchers("/sqlPage/index").access("hasRole('ROLE_ADMIN')")
-                .antMatchers("/userPage/index").access("hasRole('ROLE_ADMIN')")
-                .antMatchers("/newUserPage/index").access("hasRole('ROLE_ADMIN')")
-                .anyRequest().authenticated()
-
-                .and()
-                .formLogin()
-                .loginPage("/logInPage/index")
-                .permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/logInPage/index")
+                .antMatchers("/mainPage/")
                 .permitAll();
     }
 
@@ -58,8 +40,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return authenticationManager();
     }
 
-    @Autowired
-    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-       auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-    }
+//    @Autowired
+//    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//       auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+//    }
 }
