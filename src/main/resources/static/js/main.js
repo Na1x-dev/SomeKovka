e2c3e1725ee58722199ed3a8f031d34b90558c37
na1x <na1x.1024@gmail.com>
2023-01-26 12:34:19 +0300

    added animation styles

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,19 +1,44 @@
-let pages = document.getElementsByClassName('page');
+let pages = [].slice.call(document.getElementsByClassName('page'));
 let counter = 0;
 let scrollStatus = true;
 let headerButtons = [].slice.call(document.getElementsByClassName('header-button'));
 let defaultStyleHeaderButton = `
-    font-weight: 300;
+    font-weight: 400;
     border-bottom: 1px solid #ededed;`;
 let defaultStyleHeaderButtonHover = `
     border-bottom: 1px solid #444444;
     cursor: pointer;
-    font-weight: 300;`;
+    font-weight: 400;`;
 let selectedStyleHeaderButton = `
     font-weight: 700;
     border-bottom: 1px solid #ededed;`;
 let selectedStyleHeaderButtonHover = `
     cursor: default;`;
+
+let selectedStyleHeaderButtonAnimSize = `
+        animation-duration: 0.5s;
+        animation-name: textSizeUp;
+        animation-fill-mode: forwards;`;
+
+let selectedStyleHeaderButtonAnimReverseSize = `
+        animation-duration: 0.5s;
+        animation-name: textSizeDown;
+        animation-fill-mode: forwards;`;
+
+let selectedPageAnim = `
+        animation-duration: 0.5s;
+        animation-name: pageShowUp;
+        animation-fill-mode: forwards;`;
+
+let selectedPageAnimReverse = `
+        animation-duration: 0.5s;
+        animation-name: pageShowDown;
+        animation-fill-mode: forwards;`;
+
+let initAnimationStyle = `
+animation-duration: 1.5s;
+animation-name: initAnim;
+animation-fill-mode: forwards;`;
 
 function setHeaderButtonsOnClick() {
     headerButtons.map(function (headerButton, index) {
@@ -45,41 +70,51 @@ function setOnWheel() {
     }
 }
 
+
 function displayPage() {
-    [].slice.call(pages).map(function (page) { page.hidden = true; });
-    pages[counter].hidden = false;
-    updateHeaderButtons();
+    // [].slice.call(pages).map(function (page) { page.hidden = true; });
+    // pages[counter].hidden = false;
+    updateHeaderButtonsAnim();
+    updatePagesAnim();
 }
 
-function updateHeaderButtons() {
 
-
-
+function updateHeaderButtonsAnim() {
     headerButtons.map(function (headerButton, index) {
-        headerButton.style = defaultStyleHeaderButton;
-        headerButton.onmouseover = function () {
-            this.style = defaultStyleHeaderButtonHover;
+        if (getComputedStyle(headerButton).fontSize > "25px" && index != counter) {
+            headerButton.style = selectedStyleHeaderButtonAnimReverseSize;
         }
-        headerButton.onmouseout = function () {
-            this.style = defaultStyleHeaderButton;
-        }
-        if (index == counter) {
-            headerButton.style = selectedStyleHeaderButton;
-            headerButton.onmouseover = function () {
-                this.style = selectedStyleHeaderButton;
-            }
-            headerButton.onmouseout = function () {
-                this.style = selectedStyleHeaderButton;
-            }
-        }
-    });
 
+        else if (getComputedStyle(headerButton).fontSize < "30px" && index == counter) {
+            headerButton.style = selectedStyleHeaderButtonAnimSize;
+        };
+    });
+}
+
+function updatePagesAnim() {
+    pages.map(function (page, index) {
+        if (getComputedStyle(page).height > "0" && index != counter) {
+            page.style = selectedPageAnimReverse;
+        }
+
+        else if (getComputedStyle(page).height < "100vh" && index == counter) {
+            page.style = selectedPageAnim;
+        };
+    });
+}
+
+function initAnimation(){
+    let body = document.querySelector('body');
+    body.style = initAnimationStyle;
 }
 
 function main() {
+    initAnimation();
+    headerButtons.map(function (headerButton, index) { headerButton = defaultStyleHeaderButton; });
     displayPage();
     setHeaderButtonsOnClick();
     setOnWheel();
 }
 
 main();
+
